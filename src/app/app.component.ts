d635c3ac8c932b79e6169120fad77e290005cba8
dikend <dikendwd@gmail.com>
2017-09-22 14:59:20 +0800

    update

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,23 @@
-import { Component } from '@angular/core';
-
+import { Component, OnInit } from '@angular/core';
+import * as AV from 'leancloud-storage';
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: []
 })
-export class AppComponent {
+export class AppComponent implements OnInit {
   title = 'app works!';
+
+  ngOnInit() {
+    console.log(AV);
+
+    //  console.log(this.av)
+    // var TestObject = AV.Object.extend('TestObject');
+    // var testObject = new TestObject();
+    // testObject.save({
+    //   words: 'Hello World!'
+    // }).then(function(object) {
+    //   alert('LeanCloud Rocks!');
+    // })
+  }
 }
--- src/modules/login/login.component.ts
+++ src/modules/login/login.component.ts
@@ -10,9 +10,6 @@ export class DefindexComponent implements OnInit {
   ngOnInit() {
     console.log(AV);
 
-    const appId = 'FiwsYyo5ilGwbj1NJ1b2Ub3c-gzGzoHsz';
-    const appKey = 'ALC3hN40oHBH7Fke3RJXvvsO';
-    AV.init({ appId, appKey });
     //  console.log(this.av)
     // var TestObject = AV.Object.extend('TestObject');
     // var testObject = new TestObject();
