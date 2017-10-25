2ade3ae6e6d4c690574010c79c40a86b9c691ca0
frontlich <frontlich@frontlich.com>
2017-10-25 11:35:51 +0800

    :sparkles:

--- src/modules/basetpl/components/directives/stopParentClick/stop-parent-click.component.ts
+++ src/modules/basetpl/components/directives/stopParentClick/stop-parent-click.component.ts
@@ -3,6 +3,7 @@ import { flyIn } from 'animations/fly-in';
 
 @Component({
   templateUrl: './stop-parent-click.component.html',
+  styleUrls: ['./stop-parent-click.component.scss'],
   animations: [
     flyIn
   ]
@@ -10,5 +11,17 @@ import { flyIn } from 'animations/fly-in';
 export class StopParentClickDemoComponent {
 
   constructor(){}
+
+  eventList: string[] = [];
+
+  parentClick() {
+    this.eventList.push('父级事件被触发了');
+    setTimeout(()=>{this.eventList.shift()}, 2000);
+  }
+
+  childClick() {
+    this.eventList.push('子级事件被触发了');
+    setTimeout(()=>{this.eventList.shift()}, 2000);
+  }
 
 }
