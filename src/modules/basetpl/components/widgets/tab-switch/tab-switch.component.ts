bb71cf66e04deef7fe118a4ace15fc0bfa22fec7
frontlich <frontlich@frontlich.com>
2017-10-26 14:21:30 +0800

    :art:

--- src/modules/basetpl/components/widgets/tab-switch/tab-switch.component.ts
+++ src/modules/basetpl/components/widgets/tab-switch/tab-switch.component.ts
@@ -11,10 +11,24 @@ import { flyIn } from 'animations/fly-in';
 
 export class TabSwitchDemoComponent implements OnInit {
 
+  tabList: {id:string,name: string}[] = [{name:'abc',id: 'abc'},{name:'def',id: 'def'},{name:'ghi',id: 'ghi'}];
+  contList: {id:string,content: string}[] = [{content:'abc',id: 'abc'},{content:'def',id: 'def'},{content:'ghi',id: 'ghi'}];
+  count: number = 0;
+
   constructor() {}
 
   ngOnInit() {
+  }
 
+  deleteItem(i) {
+    this.tabList.splice(i,1);
+    this.contList.splice(i,1);
+  }
+
+  addItem() {
+    this.count++;
+    this.tabList.push({name: `newTab${this.count}`, id: `newId${this.count}`});
+    this.contList.push({content: `newContent${this.count}`, id: `newId${this.count}`});
   }
 
 }
