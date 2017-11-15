31e9f45f4a83222a1da491c6ec90877b7f543183
frontlich <frontlich@frontlich.com>
2017-11-15 11:10:47 +0800

    :tada:

--- src/modules/basetpl/components/code-review/code-review.component.ts
+++ src/modules/basetpl/components/code-review/code-review.component.ts
@@ -1,5 +1,22 @@
-import { Component} from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { Http } from '@angular/http';
 import { flyIn } from 'animations/fly-in';
+import * as moment from 'moment';
+
+import { environment } from 'environments/environment';
+
+class IsRows {
+  filename:string;
+  userid:string;
+  username:string;
+}
+
+class Record {
+  createdAt: string;
+  objectId: string;
+  userid: string;
+  isrows: IsRows[];
+}
 
 @Component({
   templateUrl: './code-review.component.html',
@@ -8,11 +25,61 @@ import { flyIn } from 'animations/fly-in';
     flyIn
   ]
 })
-export class CodeReviewComponent {
-  recordList = [{date: '2017-11-09', name: '王迪'}];
-  today: Date = new Date();
-  DC_FES = ['王迪', '隗合飞', '郝建阳', '郑柳燕', '穆红敏', '李晨', '田玉平', '齐少青', '杨贵林'];
+export class CodeReviewComponent implements OnInit {
+  today: string;
+  loading: boolean;
+  objId: string;
+  recordList: Record[];
+  recordItem: Record = new Record();
+  users: any[];
 
-  constructor(){}
+  constructor(private http: Http){}
 
+  ngOnInit() {
+    this.today = moment().format('YYYY-MM-DD');
+    this.recordItem.isrows = [new IsRows()];
+    this.http.get(environment.server + 'users').toPromise().then(response => response.json()).then(data => {
+      this.users = data.results;
+    })
+    this.getData();
+  }
+
+  getData() {
+    this.loading = true;
+    this.http.get(environment.server + 'classes/Cr').toPromise().then(response => response.json()).then(data => {
+      this.loading = false;
+      this.recordList = data.results;
+console.log(data)
+      let lastRecord = this.recordList[this.recordList.length - 1];
+      if(moment(lastRecord.createdAt).format('YYYY-MM-DD') === this.today) {
+        this.objId = lastRecord.objectId;
+      }
+    })
+  }
+
+  postData() {
+    this.http.post(environment.server + 'classes/Cr', this.recordItem).toPromise().then(response => response.json()).then(data => {
+      this.getData();
+    })
+  }
+
+  putData() {
+    this.http.put(environment.server + 'classes/Cr/' + this.objId, this.recordItem).toPromise().then(response => response.json()).then(data => {
+      this.getData();
+    })
+  }
+
+  chooseUser() {
+    this.recordItem.isrows.forEach(item => {
+      item.username = this.users.filter(p => p.objectId === item.userid)[0].nickname;
+    })
+  }
+
+  deleteIsRows(i) {
+    this.recordItem.isrows.splice(i,1);
+  }
+
+  addIsRows() {
+    this.recordItem.isrows.push(new IsRows());
+  }
 }
