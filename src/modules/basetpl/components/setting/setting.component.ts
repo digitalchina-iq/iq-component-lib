1c49024001be07bc9cfd573dbe26033927740d53
dikend <dikendwd@gmail.com>
2017-12-08 17:31:18 +0800

    新功能

--- src/modules/basetpl/components/setting/setting.component.ts
+++ src/modules/basetpl/components/setting/setting.component.ts
@@ -1,5 +1,9 @@
 import { Component} from '@angular/core';
 import { flyIn } from 'animations/fly-in';
+import { environment } from 'environments/environment';
+
+import { Http } from '@angular/http';
+import { WindowService } from 'core';
 
 @Component({
   templateUrl: './setting.component.html',
@@ -9,10 +13,63 @@ import { flyIn } from 'animations/fly-in';
 })
 
 export class SettingComponent {
-  a;
-  b;
-  c;
 
-  constructor(){}
+  date: any[]=[];
+  
+  uploadApi: string;
+  
+  imgSrc: string;
+
+  userInfo: {}=JSON.parse(window.localStorage.getItem('userinfo'));
+
+  constructor(private iqhttp: Http, private windowService: WindowService){}
+
+  ngOnInit() {
+    this.uploadApi = environment.server + 'files/upload';
+    // this.userInfo = JSON.parse(window.localStorage.getItem('userinfo'));
+    this.imgSrc=this.userInfo["headurl"];
+    console.log(this.userInfo);
+    console.log(this.userInfo["nickname"]);
+
+    // this.iqhttp.get(environment.server+"/classes/_file/"+this.userInfo.headid)
+    // .map(res => res.json())
+    // .subscribe(res => {
+    //   this.imgSrc=res.url;
+    // })
+
+    // this.imgSrc=window.
+    // this.iqhttp.put(environment.server+"/classes/Product/59cf01dd67f356003a61f9a1",
+    // {
+    //         "price":78
+    //       },
+    // )
+    // .map(res => res.json())
+    // .subscribe(res => {
+    //     console.log("+++++");
+    //     console.log(res)
+    //     console.log("+++++");
+    //     // this.router.navigate(["/index"]);
+    // })
+
+
+  }
+
+  fileUploadSuccess(data){
+    console.log(data);
+    this.imgSrc = data.url;
+
+    this.iqhttp.put(environment.server+"/users/"+this.userInfo["objectId"],{
+      "headurl":data.url
+    })
+    .map(res => res.json())
+    .subscribe(res => {})
+
+
+  }
+
+  deleteItem(i){
+    console.log(i);
+  }
+
 
 }
--- src/shared/components/frame-components/header.component.ts
+++ src/shared/components/frame-components/header.component.ts
@@ -12,7 +12,7 @@ export class HeaderComponent implements OnInit {
   scolor: string= window.localStorage.getItem("scolor")==null?"#444":window.localStorage.getItem("scolor");
   ecolor: string= window.localStorage.getItem("ecolor")==null?"#20A8E8":window.localStorage.getItem("ecolor");
   nickname: string= JSON.parse(localStorage.getItem("userinfo"))==null?"未知":JSON.parse(localStorage.getItem("userinfo")).nickname;
-
+  headurl : string= JSON.parse(window.localStorage.getItem("userinfo")).headurl==null?"assets/img/user-profile-pic-0.png":JSON.parse(window.localStorage.getItem("userinfo")).headurl;
   constructor(private router: Router) {
     
   }
@@ -23,12 +23,12 @@ export class HeaderComponent implements OnInit {
 
   scolorm(e){
     window.localStorage.setItem("scolor",this.scolor);
-    $("body").css("background","linear-gradient(135deg,"+this.scolor+" 0,"+this.ecolor+" 100%)");
+    $("#bodydivload").css("background","linear-gradient(135deg,"+this.scolor+" 0,"+this.ecolor+" 100%)");
   }
 
   ecolorm(e){
     window.localStorage.setItem("ecolor",this.ecolor);
-    $("body").css("background","linear-gradient(135deg,"+this.scolor+" 0,"+this.ecolor+" 100%)");
+    $("#bodydivload").css("background","linear-gradient(135deg,"+this.scolor+" 0,"+this.ecolor+" 100%)");
   }
 
   logout(){
